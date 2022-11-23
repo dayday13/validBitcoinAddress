25f862272182b42dd0ad373bac8da2af3d77960f
dayday13 <118615965+dayday13@users.noreply.github.com>
2022-11-23 22:55:24 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# @validBitcoinAddress
+This is a telegram bot that allows the user to check for a bitcoin address balance.
+
+## General Information
+* The bot takes a Bitcoin address or a QR photo and checks the address balance.
+* It only takes one address at a time.
+
+
+## Usage
+*To start run `python main.py` in the project folder.
+
+## Project Status
+* Project was built on a Windows environment.
---
+++ telegram/BotCommands.py
@@ -0,0 +1,54 @@
+class BotCommand:
+    
+    ''' This class is a bot command, the class contains the bot so to acces the
+    the username info of the bot, the command is associated with a function,
+    and a help message'''
+    
+    def __init__(self, bot, command, function=None, help_message=""):
+        self.bot = bot
+        self.command = command
+        self.function = function
+        self.help_message = help_message
+        
+    def transform_message_text(self, text):
+        if text == self.command:
+            return self.command
+        
+        if text == self.command + "@" + self.bot.info.username:
+            return self.command
+        
+    
+    def fire(self, params):
+        self.function(*params)
+    
+        
+    def __eq__(self, text):
+        
+        text = text.lower()
+        
+        if text.startswith(self.command):
+            return True
+        
+        if text.startswith(self.command + "@" + self.bot.info.username.lower()):
+            return True
+        
+        return False
+
+# =============================================================================
+# Bot Command List
+# =============================================================================
+
+class BotCommandList:
+    
+    ''' This class manages a set of commands'''
+    
+    def __init__(self):
+        self.commands = {}
+    
+    def __getitem__(self, key):
+        return self.commands[key]
+        
+    def add_command(self, bot, command, function, help_message):
+        self.commands[command] = BotCommand(bot, command, function, help_message)
+        
+    
